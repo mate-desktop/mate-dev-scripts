a45434bef2471ba029593717a0be98d4e470767e
Steve Zesch <stevezesch2@gmail.com>
2011-12-22 22:53:01 -0500

    Added script to update all git repos in one go.

---
+++ update.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+# Author: Steve Zesch
+
+# This script assumes that the repos are locally organized
+# as some_parent_folder/mate_repo. For instance, my cloned repos
+# are stored as mate-desktop/mate-common, mate-desktop/mate-doc-utils, etc.
+
+# Obviously, you want the cwd when the script runs to be the folder that
+# contains all the mate repos. You can achieve this by copying this 
+# script from mate-dev-scripts to the parent directory, running the
+# script via it's path (mate-dev-scripts/update.py), or by creating a
+# symbolic link. I prefer the symbolic link method. It doesn't matter,
+# so long as the cwd for this script is the parent directory of all
+# the mate repos.
+
+import os
+
+# update all the directories
+for d in os.listdir('.'):
+	if os.path.isdir(d):
+		os.chdir(d)
+		if '.git' in set(os.listdir('.')):
+			os.system('git pull')
+		os.chdir('..')
