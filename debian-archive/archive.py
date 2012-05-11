a73529b1f9e7734540d0dbf9744eb1eea21db7d8
Steve Zesch <stevezesch2@gmail.com>
2012-05-11 12:24:46 -0400

    Automated handling git branches for archiving.

--- debian-archive/archive.py
+++ debian-archive/archive.py
@@ -2,10 +2,12 @@
 import optparse
 import os
 import sys
+import subprocess
 
 parser = optparse.OptionParser()
 parser.add_option("-v", "--version", dest="version", help="set upstream version")
 parser.add_option("-o", "--orig", dest="orig", action="store_true", help="rename file to use with debuild")
+parser.add_option("-b", "--branch", dest="branch", help="set the branch to archive")
 (options, args) = parser.parse_args()
 
 if len(args) == 0:
@@ -29,6 +31,33 @@ if options.version is None:
     print "E: Package version not set!"
     sys.exit(1)
 
+# checkout a different branch, if the user chose to do so
+if options.branch:
+	os.chdir(package)
+
+	# get the current branch
+	try:
+		output = subprocess.check_output(["git", "branch"]).split("\n")
+		
+		for line in output:
+			if line[0] == "*":
+				current_branch = line[2:]
+				break
+
+	except subprocess.CalledProcessError:
+		print "E: Unable to get a list of branches for this directory."
+		sys.exit(1)
+
+	# checkout the desired branch
+	try:
+		ret_val = subprocess.check_output(["git", "checkout", options.branch], stderr = sys.stdout).rstrip("\n")
+		print "I: {}".format(ret_val)
+	except subprocess.CalledProcessError:
+		print "E: Could not checkout branch {} make sure it exists.".format(options.branch)
+		sys.exit(1)
+
+	os.chdir("..")	
+
 version = options.version
 
 print "I:", package, options.version
@@ -44,3 +73,14 @@ os.system("rm -rf %(package)s-%(version)s/" % {"package": package, "version": ve
 if options.orig:
     #os.system("mv %(package)s-%(version)s.tar.bz2 %(package)s_%(version)s.orig.tar.bz2" % {"package": package, "version": version})
     os.system("mv %(package)s-%(version)s.tar.xz %(package)s_%(version)s.orig.tar.xz" % {"package": package, "version": version})
+
+# change the branch back to what it was prior to running this script
+if options.branch:
+	os.chdir(package)
+
+	try:
+		ret_val = subprocess.check_output(["git", "checkout", current_branch], stderr = sys.stdout).rstrip("\n")
+		print "I: {}".format(ret_val)
+	except subprocess.CalledProcessError:
+		print "E: Could not checkout the original branch. Current branch is {}".format(options.branch)
+		sys.exit(1)
