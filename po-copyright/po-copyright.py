2c4946df31a44926734293bd2854f3e95b0e5f7a
Steve Zesch <stevezesch2@gmail.com>
2012-07-15 20:56:16 -0400

    Created script to automatically backup gnome copyrights from *.po files.

---
+++ po-copyright/po-copyright.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+HEADER_FORMAT = "========== {} ==========\n"
+
+# Make sure the correct number of args were given.
+if len(sys.argv) < 1:
+    print "Please specify the path to po/"
+    sys.exit(1)
+    
+os.chdir(sys.argv[1])
+
+copyright = open("gnome-copyrights.txt", "w")
+
+files = [f for f in sorted(os.listdir(os.curdir)) if f[-3:] == ".po"]
+
+for f in files:
+    po = open(f, "r")
+        
+    copyright.write(HEADER_FORMAT.format(f))
+    
+    line = po.readline()
+    while line.startswith("#"):
+        copyright.write(line)
+        line = po.readline()
+        
+    copyright.write("\n\n\n\n")
+
+
+
+
+
+ 
+    
