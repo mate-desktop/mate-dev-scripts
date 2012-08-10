11867009902fb41c137c0ce003c38f5acec73c6b
Steve Zesch <stevezesch2@gmail.com>
2012-08-10 16:51:58 -0400

    More incorrect fsf address support.

--- fix-fsf-address/fixlicense.sh
+++ fix-fsf-address/fixlicense.sh
@@ -17,6 +17,7 @@ while read line; do
     sed -i "s/\(.*\)675 Mass Ave, Cambridge\(.*\)/\151 Franklin St, Fifth Floor, Boston\2/" $line
     sed -i "s/\(.*\)02111-1307\(.*\)/\102110-1301\2/" $line
     sed -i "s/\(.*\)Boston, MA 02139\(.*\)/\1Boston, MA 02110-1301\2/" $line
+    sed -i "s/\(.*\)Cambridge, MA 02139\(.*\)/\1Boston, MA 02110-1301\2/" $line
 done < $FILE
 
 rm $FILE
