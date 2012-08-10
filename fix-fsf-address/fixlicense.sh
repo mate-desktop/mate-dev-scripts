4827837f97e9f28d4d6860d71588d094b1e560ed
Steve Zesch <stevezesch2@gmail.com>
2012-08-09 21:30:03 -0400

    fixlicense.sh now handles more incorrest fsf addresses.

--- fix-fsf-address/fixlicense.sh
+++ fix-fsf-address/fixlicense.sh
@@ -13,7 +13,10 @@ cd $1
 
 while read line; do
     sed -i "s/\(.*\)59 Temple Place - Suite 330\(.*\)/\151 Franklin St, Fifth Floor\2/" $line
+    sed -i "s/\(.*\)59 Temple Place, Suite 330\(.*\)/\151 Franklin St, Fifth Floor\2/" $line
+    sed -i "s/\(.*\)675 Mass Ave, Cambridge\(.*\)/\151 Franklin St, Fifth Floor, Boston\2/" $line
     sed -i "s/\(.*\)02111-1307\(.*\)/\102110-1301\2/" $line
+    sed -i "s/\(.*\)Boston, MA 02139\(.*\)/\1Boston, MA 02110-1301\2/" $line
 done < $FILE
 
 rm $FILE
